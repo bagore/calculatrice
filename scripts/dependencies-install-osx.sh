f3a08a2e7c29252a234b53c3802ae09b5bb08e61
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-03-03 11:23:38 +0100

    Implementation of a default script to install dependencies on Mac CI VM.

---
+++ scripts/dependencies-install-osx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# To stop at first error
+set -e
+
+# To trace executed commands
+set -x
+
+
+
+#
+#   Installation of needed dependencies
+#
+brew update
+
+#brew install <package>
+
+
+
+exit $?
