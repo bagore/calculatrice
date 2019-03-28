4e7cc2138e3e17be03b3b7086ea89b10afc0662e
UNICE_ETU/BAA <alan.batista@etu.unice.fr>
2019-03-28 23:40:09 +0100

    Model: implementation de la Pile et de ses tests.

---
+++ src/model/Pile.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+
+
+/* ------------- Declaration de la classe Pile ----------- */
+
+
+
+class Pile
+{
+    /* Declaration des membres privés */
+    std::stack<double> m_pile;
+
+public:
+
+    Pile(); //Constructeur par defaut
+    void empiler(const double &inValue);
+    double depiler();
+    void vider();
+    size_t nombreElements() const;
+    bool estVide() const;
+};
